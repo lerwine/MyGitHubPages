1c27e5f2ca7bdbe92413d74b55d4bb561abd19be
lerwine <lenny@erwinefamily.net>
2018-08-12 01:27:36 -0400

    Removed legacy files

--- legacy/bin/script/app.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="angular" />
-interface IMenuConfigSettings {
-}
-interface IMainModuleScope extends angular.IScope {
-}
-//# sourceMappingURL=app.d.ts.map
--- legacy/bin/script/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-angular.module("main", ['ngRoute'])
-    .controller("pageProperties", ['$scope', '$http', '$route', function ($scope, $http, $route) {
-        $http.get("menuSchema.json").then(function (value) {
-        });
