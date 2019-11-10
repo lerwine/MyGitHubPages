7537d2ccf6a58139820a1b13b008653c51717bd9
lerwine <lenny@erwinefamily.net>
2019-11-09 23:09:47 -0500

    Replaced pageTitleService with pageLocationService

--- script/postbuild.js
+++ script/postbuild.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const cpx = require("cpx");
-cpx.copySync("../src/{*.js,*.css,*.ico,*.html}", '../gh-pages', { clean: true });
+cpx.copySync("../src/{*.js,*.css,*.ico,*.html,*.htm}", '../gh-pages', { clean: true });
 cpx.copySync("../src/lib/**/*", '../gh-pages/lib', { clean: true });
-['home', 'colorBuilder', 'regexTester', 'uriBuilder'].forEach((n) => {
+['colorBuilder', 'regexTester', 'uriBuilder'].forEach((n) => {
     cpx.copySync("../src/" + n + '/**/{*.js,*.htm}', '../gh-pages/' + n, { clean: true });
 });
 cpx.copySync('../src/images/**/*', '../gh-pages/images', { clean: true });
--- script/postbuild.ts
+++ script/postbuild.ts
@@ -1,8 +1,8 @@
 ﻿import * as cpx from 'cpx';
 
-cpx.copySync("../src/{*.js,*.css,*.ico,*.html}", '../gh-pages', { clean: true });
+cpx.copySync("../src/{*.js,*.css,*.ico,*.html,*.htm}", '../gh-pages', { clean: true });
 cpx.copySync("../src/lib/**/*", '../gh-pages/lib', { clean: true });
-['home', 'colorBuilder', 'regexTester', 'uriBuilder'].forEach((n: string) => {
+['colorBuilder', 'regexTester', 'uriBuilder'].forEach((n: string) => {
     cpx.copySync("../src/" + n + '/**/{*.js,*.htm}', '../gh-pages/' + n, { clean: true });
 });
 cpx.copySync('../src/images/**/*', '../gh-pages/images', { clean: true });
